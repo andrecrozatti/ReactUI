61b09d566b03e75055fe64c9851b6625a8fcc217
andrecrozatti <andrecrozatt12@gmail.com>
2023-09-03 22:16:07 -0300

    Configurando a primeira rota

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,16 @@
-export {};
+import {Routes , Route, Navigate} from 'react-router-dom'
+import React from 'react';
+
+export const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path='/pagina-inicial' element={<p>Página Inicial</p>}/>
+                
+            
+
+            <Route path='*' element={<Navigate to="/pagina-inicial" />} />
+              
+        </Routes>
+    );
+};
