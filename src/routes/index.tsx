fc77fa4ef8ac33614477a545285ca7d9feaaac06
andrecrozatti <andrecrozatt12@gmail.com>
2023-09-13 07:35:34 -0300

    Criação do tema dark e botão para alterar o tema

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import {Routes , Route, Navigate} from 'react-router-dom'
 import {Button} from '@mui/material'
 import React from 'react';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
 
+    const {toggleTheme} = useAppThemeContext()
     return (
         <Routes>
-            <Route path='/pagina-inicial' element={<Button variant='contained' color='primary'>Teste</Button>}/>
+            <Route path='/pagina-inicial' element={<Button variant='contained' color='primary' onClick={toggleTheme}>Teste</Button>}/>
                 
             
 
