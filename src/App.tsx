be22f73b52b1c0493f8fd3666796128f6d409dbc
andrecrozatti <andrecrozatt12@gmail.com>
2023-09-13 06:45:06 -0300

    Criação do tema light

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-
+import { ThemeProvider } from "@emotion/react";
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
-}
-
+};
